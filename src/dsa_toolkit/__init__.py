b4b09b8c931ff07fc1bf7ab3c56fb2cf04e71eb1
Victor <victorlopezlopez93@gmail.com>
2026-01-19 17:45:29 +0000

    feat: add deque implementation with circular buffer and tests

--- src/dsa_toolkit/__init__.py
+++ src/dsa_toolkit/__init__.py
@@ -1,4 +1,12 @@
 from .stack import Stack, EmptyStackError
 from .queue import Queue, EmptyQueueError
+from .deque import Deque, EmptyDequeError
 
-__all__ = ["Stack", "EmptyStackError", "Queue", "EmptyQueueError"]
+__all__ = [
+    "Stack",
+    "EmptyStackError",
+    "Queue",
+    "EmptyQueueError",
+    "Deque",
+    "EmptyDequeError",
+]
